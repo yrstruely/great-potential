6e5baea6935547e8b21e8c1771faaa1f8de48045
Kerry Harris <kerry.harris@vodafone.com>
2015-09-24 21:10:09 +1200

    Added recall with sql db

---
+++ recall.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'shotgun'
+require 'data_mapper'
+
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+ 
+class Note
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+ 
+DataMapper.finalize.auto_upgrade!
+
+
+get '/' do
+  @notes = Note.all :order => :id.desc
+  @title = 'All Notes'
+  erb :home
+end
